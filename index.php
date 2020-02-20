ea15e6d8beb2d9c4d7c0b36d7f7c329a8760bf79
MyCrisis <thecrisis1409@gmail.com>
2020-02-20 11:20:56 -0600

    abre el curriculum por 5ta o sexta vez

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cardenas Morales José María-Curriculum</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	<nav>
+		<img src="foto_mia.png" class="Redonda" align=left height="340px" width="450px" border="5rem">
+		<H1>
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<font size="7">
+				Cardenas Morales Jose Maria.
+			</font>
+		</H1>
+		<H2>
+			Estudiante de programacion/ Gamer experto
+		</H2>
+		<br><br>
+		<a href="https://www.cecyt9.ipn.mx/">
+			<img src="cecyt9.jpg" height="40px" width="40px">
+		</a>
+		<H4>
+			<a href="https://www.cecyt9.ipn.mx/">
+				CECyT 9 "Juan de dios batiz".
+			</a>
+		</H4>
+	</nav>
+	<div class="shuriken_dos" width="10px" height="10px">
+		<img src="shuriken.png">
+	</div>
+	<section>
+		<article>
+			<H2>
+				Acerca de<br><br>
+			</H2>
+			<H4>
+				Estudiante de programacion con experiencia en el uso de aplicaciones de Microsoft (Microsoft Word, Microsoft PowerPoint y Microsoft Excel). Con experiencia en la codificacion de programas en java utilizando aplicaciones como sublime text o NetBeans. Con conocimiento sobresaliente en el diseño de páginas Web utilizando DreamWeaver y mostrando dominio en trabajos individuales. Estudiante en el Instituto Politécnico Nacional cursando el cuarto semestre.
+			</H4>
+		</article>
+	</section>
+	<div class="shuriken_uno" width="10px" height="10px">
+		<img src="shuriken.png">
+	</div>
+	<section>
+		<article>
+			<div class="SIN_EXPERIENCIA">
+				<H2>
+					Experiencia<br><br>
+				</H2>
+				<H4>
+					*sin experiencia
+				</H4>
+			</div>
+		</article>
+	</section>
+	<div class="shuriken_dos" width="10px" height="10px">
+		<img src="shuriken.png">
+	</div>
+	<section>
+		<article>
+			<H2>
+				Educacion<br><br>
+			</H2>
+			<a href="https://www.justo-sierra.edu.mx/">
+				<img src="justo_sierra.jpg" height="100px" width="100px">
+			</a>
+			<H3>
+				<a href="https://www.justo-sierra.edu.mx/">
+					Justo Sierra:<br>
+				</a>
+			</H3>
+			<H4>
+				Tronco comun<br>
+				Educacion primaria:<br>
+				2012-2015
+			</H4>
+			<br>
+			<H4>
+				Tronco comun<br>
+				Educacion secundaria:<br>
+				2015-2018
+			</H4>
+			<br><br>
+			<a href="https://www.cecyt9.ipn.mx/">
+				<img src="cecyt9.jpg" height="100px" width="100px">
+			</a>	
+			<H3>
+				<a href="https://www.cecyt9.ipn.mx/">
+					Instituto Politecnico Nacional
+				</a>
+			</H3>
+			<H4>
+				Tecnico en programacion<br>
+				Preparatoria<br>
+				2018-Actualidad
+			</H4>
+		</article>
+	</section>
+	<div class="shuriken_uno" width="10px" height="10px">
+		<img src="shuriken.png">
+	</div>
+	<section>
+		<H2>
+			Educacion
+		</H2>
+		<H3>
+			Español<br>
+		</H3>
+		<H4>
+			Competencia bilingue o nativa
+		</H4>
+		<br><br>
+		<H3>
+			Ingles<br>
+		</H3>
+		<H4>
+			Competencia medio-superior
+		</H4>
+	</section>
+</body>
+</html>
